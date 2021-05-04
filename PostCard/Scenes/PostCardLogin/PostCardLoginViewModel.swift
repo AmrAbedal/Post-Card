6bea295859c27b046018be36ee2c9105730f9002
Amr Abdelwhab <amr.abdelwahab@warbabank.com>
2021-05-04 20:32:40 +0200

    add email and passwords test field

--- PostCard/Scenes/PostCardLogin/PostCardLoginViewModel.swift
+++ PostCard/Scenes/PostCardLogin/PostCardLoginViewModel.swift
@@ -12,4 +12,8 @@ class PostCardLoginViewModel {
     init(coordinator: PostCardLoginCoordinator) {
         self.coordinator = coordinator
     }
+    func signInButtonTapped() {
+        
+        
+    }
 }
